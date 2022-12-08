0be890d346990b76238eae7778f77063406562f2
Abubakar Musa <shaunaibek@gmail.com>
2022-12-08 09:26:15 +0100

    update

--- app.js
+++ app.js
@@ -168,18 +168,36 @@ const y = 11;
 
 // console.log(person1.getFullName());
 
-const companies = [
-  { name: "comapny 1", category: "Finance", start: "1980", end: "2000" },
-  { name: "comapny 2", category: "Retail", start: "1960", end: "2000" },
-  { name: "comapny 3", category: "Technology", start: "1990", end: "2000" },
-  { name: "comapny 4", category: "Retail", start: "1920", end: "2000" },
-  { name: "comapny 5", category: "Technology", start: "1940", end: "2000" },
-  { name: "comapny 6", category: "Retail", start: "1950", end: "2000" },
-  { name: "comapny 7", category: "Technology", start: "1967", end: "2000" },
-  { name: "comapny 8", category: "Finance", start: "1983", end: "2000" },
-  { name: "comapny 9", category: "Auto", start: "1973", end: "2000" },
-  { name: "comapny 10", category: "Auto", start: "170", end: "2000" },
-  { name: "comapny 11", category: "Auto", start: "1509", end: "2000" },
+let companies = [
+  { name: "comapny 1", category: "Finance", start: "1980", end: "2000", id: 1 },
+  { name: "comapny 2", category: "Retail", start: "1960", end: "2000", id: 2 },
+  {
+    name: "comapny 3",
+    category: "Technology",
+    start: "1990",
+    end: "2000",
+    id: 3,
+  },
+  { name: "comapny 4", category: "Retail", start: "1920", end: "2000", id: 4 },
+  {
+    name: "comapny 5",
+    category: "Technology",
+    start: "1940",
+    end: "2000",
+    id: 5,
+  },
+  { name: "comapny 6", category: "Retail", start: "1950", end: "2000", id: 6 },
+  {
+    name: "comapny 7",
+    category: "Technology",
+    start: "1967",
+    end: "2000",
+    id: 7,
+  },
+  { name: "comapny 8", category: "Finance", start: "1983", end: "2000", id: 8 },
+  { name: "comapny 9", category: "Auto", start: "1973", end: "2000", id: 9 },
+  { name: "comapny 10", category: "Auto", start: "170", end: "2000", id: 10 },
+  { name: "comapny 11", category: "Auto", start: "1509", end: "2000", id: 11 },
 ];
 
 const ages = [33, 48, 13, 16, 19, 25, 39, 40, 23, 56, 78, 96];
@@ -235,14 +253,108 @@ const canDrink = ages.filter((age) => {
 // };
 // log(10, 11, "-");
 
-let name = prompt("Enter name");
-let age = prompt("Enter Age");
-let isFacemask = prompt("Are You wearing facemask");
+// let name = prompt("Enter name");
+// let age = prompt("Enter Age");
+// let isFacemask = prompt("Are You wearing facemask");
 
-if (age > 12 && isFacemask === "yes") {
-  alert(`Welcome Mr/Mrs ${name} to my mall`);
-} else if (age > 12 && isFacemask === "no") {
-  alert(`Mr/Mrs ${name} you need a face mask to enter`);
-} else {
-  alert(`you are below age to enter`);
-}
+// if (age > 12 && isFacemask === "yes") {
+//   alert(`Welcome Mr/Mrs ${name} to my mall`);
+// } else if (age > 12 && isFacemask === "no") {
+//   alert(`Mr/Mrs ${name} you need a face mask to enter`);
+// } else {
+//   alert(`you are below age to enter`);
+// }
+
+// const listItems = document.querySelectorAll(".item");
+
+// listItems.forEach((item) => {
+//   item.style.background = "red";
+// });
+
+// const ul = document.querySelector(".items");
+
+// // ul.remove();
+
+// ul.children[1].remove();
+
+// const nameForm = document.querySelector(".name-form");
+
+// nameForm.innerHTML = 'sean'
+const allUnit = [];
+
+const nameInput = document.querySelector(".input1");
+const emailInput = document.querySelector("#email1");
+const msg = document.querySelector(".msg");
+const btn = document.querySelector(".btn");
+const mylist = document.querySelector("#my-list");
+
+btn.addEventListener("click", (e) => {
+  let id = 0;
+  e.preventDefault();
+  msg.innerHTML = `welcome ${nameInput.value}`;
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "<p>pls enter all fields</p>";
+
+    setTimeout(() => {
+      msg.remove();
+    }, 3000);
+  } else {
+    if (emailInput?.value.includes("@") === false) {
+      msg.classList.add("error");
+      msg.innerHTML = "<p>pls enter a valid email</p>";
+    } else {
+      allUnit.push({
+        id: id + 1,
+        name: nameInput.value,
+        email: emailInput.value,
+      });
+      console.log(allUnit);
+      msg.classList.add("success");
+      const li = document.createElement("li");
+      allUnit.map((item) => {
+        li.innerHTML = `${item.name}: ${item.email} <i class="fa-solid fa-trash"></i>`;
+        mylist.appendChild(li);
+      });
+      msg.innerHTML = `welcome ${nameInput.value}`;
+      setTimeout(() => {
+        msg.classList.add("inactive");
+      }, 3000);
+      setTimeout(() => {
+        msg.classList.remove("inactive");
+      }, 5000);
+
+      nameInput.value = "";
+      emailInput.value = "";
+    }
+  }
+});
+
+const showCompanies = () => {
+  const companyDiv = document.querySelector(".company");
+  const need = companies.map((company) => {
+    return ` <div>
+    <p>${company.name}</p>
+    <p>${company.category}</p>
+    <p>${company.start}</p>
+    <p>${company.end}</p>
+    <button onclick=handledelete(${company.id})><i class="fa-solid fa-trash"></i></button>
+  </div> `;
+  });
+  companyDiv.innerHTML = need;
+};
+showCompanies();
+
+const handledelete = (id) => {
+  const value = companies.filter((item) => item.id === id)[0];
+  const index = companies.indexOf(value);
+
+  if (index > -1) {
+    companies.splice(index, 1);
+    showCompanies();
+  }
+  return companies;
+
+};
+
